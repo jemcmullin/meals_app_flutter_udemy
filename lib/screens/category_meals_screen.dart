bf0a04e2a86aa61ed0bc737f8b44911136fcf8aa
James <jemcmullin@gmail.com>
2021-01-24 13:05:07 -0600

    Add meal item screen and meal item cards

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -9,11 +11,23 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments;
     final id = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(id);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Text('Meals in Category'),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (context, index) {
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity);
+          }),
     );
   }
 }
---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Affordability affordability;
+  final Complexity complexity;
+
+  const MealItem(
+      {Key key,
+      @required this.title,
+      @required this.imageUrl,
+      @required this.duration,
+      @required this.affordability,
+      @required this.complexity})
+      : super(key: key);
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      default:
+        return 'Unknown';
+        break;
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Expensive';
+        break;
+      default:
+        return 'Unknown';
+        break;
+    }
+  }
+
+  void _selectedMealDetails() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _selectedMealDetails,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.black54),
+                    width: 260,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work_outline),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        complexityText,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
